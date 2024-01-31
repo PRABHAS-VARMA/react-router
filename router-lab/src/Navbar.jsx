9866e2c170980e74cd0d6a7000874506d3dcae12
PRABHAS VARMA <prabhasvarma017@gmail.com>
2024-01-31 11:11:59 +0530

    donne

--- router-lab/src/Navbar.jsx
+++ router-lab/src/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="nav">
-      <Link to="/" className="site-title">
-        Kavlium ❤️
+      <Link to="/" className="nav-t">
+        Kalvium ❤️
       </Link>
       <ul>
         <li><Link to="/Contacts">Contacts</Link></li>
